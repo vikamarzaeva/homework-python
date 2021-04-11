ed0b274cccada3187c5472bb90b5b652cc398ac5
Vika Marzaeva <marzaeva1993@gmail.com>
2021-04-11 12:52:19 +0800

    Избавилась от прямого задания ip адреса

--- get_ip_address.py
+++ get_ip_address.py
@@ -1,17 +1,12 @@
 import socket
 
 def get_ip():
-    dns={
-          "drive.google.com" : "142.250.186.78",
-          "google.com" : "64.233.162.113",
-          "mail.google.com" : "172.217.18.101"
-        }
+    dns, ip = input('').split()
+    ip_address = socket.gethostbyname(dns)
+    if ip_address == ip:
+        print(f"http://{dns} - {ip_address}")
+    else:
+        print(f"[ERROR] http://{dns} IP mismatch: {ip} {ip_address}")
 
-    for key in dns:
-        ip_address = socket.gethostbyname(key)
-        if ip_address == dns.get(key):
-            print(f"http://{key} - {ip_address}")
-        else:
-            print(f"[ERROR] http://{key} IP mismatch: {dns.get(key)} {ip_address}")
 if __name__ == '__main__':
     get_ip()
